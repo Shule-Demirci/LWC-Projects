90c5b75192037dc4895f033538811c6be2c5801a
Shule Demirci <shuledemirci@gmail.com>
2022-07-14 19:21:56 -0500

    init

---
+++ force-app/main/default/aura/Example1/alert("Hello World").js
@@ -0,0 +1,13 @@
+alert("Hello World")
+
+let js = 'amazing'
+
+if(js==='amazing') alert ('JavaScript is FUN!')
+
+js = 'boring'
+
+if(js=== 'amazing') alert ('JavaScript is FUN!')
+
+console.log('Hello World!');
+
+
---
+++ force-app/main/default/lwc/example1lwc/example1lwc.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Example1lwc extends LightningElement {}
