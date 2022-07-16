0c17a7a6eb75547fc125b9d99e568149095b94c8
Shule Demirci <shuledemirci@gmail.com>
2022-07-16 13:09:56 -0500

    Add files via upload

---
+++ teamWorkLWC/teamworkLWC.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class TeamworkLWC extends LightningElement {
+    firstName;
+    lastName;
+
+    changeHandlerFirstName(event){
+        this.firstName = event.target.value;
+    }
+
+
+    changeHandlerLastName(event){
+        this.lastName = event.target.value;
+    }
+    handleClick(){
+
+        alert("Hello " + this.firstName +' ' + this.lastName);
+    }
+}
